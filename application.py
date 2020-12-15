cedfd43f33d32ef9c9e2fa11e4e0276595fdc758
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-12-15 15:14:33 -0700

    Add house route

---
+++ application.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+import json
+
+application = Flask(__name__)
+
+
+@application.route('/houses', methods=['GET'])
+def get_houses():
+    API_KEY = os.getenv('API_KEY')
+    headers = {'x-rapidapi-key': API_KEY,
+               'x-rapidapi-host': 'realtor.p.rapidapi.com'}
+    params = request.args.to_dict()
+    houses = requests.get(
+        'https://realtor.p.rapidapi.com/properties/v2/list-for-sale', params=params, headers=headers)
+    return (houses.content)
+
+
+@application.route('/house', methods=['GET'])
+def get_house():
+    API_KEY = os.getenv('API_KEY')
+    headers = {'x-rapidapi-key': API_KEY,
+               'x-rapidapi-host': 'realtor.p.rapidapi.com'}
+    params = request.args.to_dict()
+    house = requests.get(
+        'https://realtor.p.rapidapi.com/properties/v2/detail', params=params, headers=headers)
+    return (house.content)
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
