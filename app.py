06564aa04911e62738e109a829206363d47016fe
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-12-15 02:06:23 -0700

    Add Get houses route

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/houses', methods=['GET'])
 def get_houses():
-    return 'hello world'
+    API_KEY = os.getenv('API_KEY')
+    headers = {'x-rapidapi-key': API_KEY,
+               'x-rapidapi-host': 'realtor.p.rapidapi.com'}
+    houses = requests.get(
+        'https://realtor.p.rapidapi.com/properties/v2/list-for-sale', params=request.args, headers=headers)
+    return houses
 
 
 if __name__ == '__main__':
